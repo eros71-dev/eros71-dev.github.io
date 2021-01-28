52cb9fd9507d425dd5d0bfe81e7ab8bfee05360f
eros71-dev <eros744@gmail.com>
2021-01-28 01:57:31 +0100

    Added beta of my new in-development game.
    It's about frying croquettes, has a nice dumb silly and simple MS Paint art style.

---
+++ Croquetitas Simulator/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1611795335,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/freidora-sheet0.png",
+		"images/croqueta-sheet0.png",
+		"images/croquetafria-sheet0.png",
+		"images/mesa-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/aceite-sheet0.png",
+		"images/plato-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
