a0c1ac453e2f5a0d4ff8f6758b3d1448430b7d95
eros71-dev <16540103+eros71-dev@users.noreply.github.com>
2022-05-20 04:42:59 +0200

    Add PN:UD v1

---
+++ proytecto-neutronio/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1653014544,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/pngkeycomjimmyneutronpng-sheet0.png",
+		"images/sintítulo-sheet0.png",
+		"images/ezgif5100ad84c9f-sheet0.png",
+		"images/caamara-sheet0.png",
+		"images/tilemap.png",
+		"images/ladders-sheet0.png",
+		"images/proytecto_neutronio_ultra_deluxe_logo-sheet0.png",
+		"images/galaxy-sheet0.png",
+		"media/smb3.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
